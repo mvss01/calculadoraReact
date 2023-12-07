448e75dd3c94692582b262dd138d160a70167cb8
mvss01 <marcosvictorss@hotmail.com>
2023-12-07 18:54:37 -0300

    V0.0.2

--- src/App.js
+++ src/App.js
@@ -5,22 +5,37 @@ import { Container, Content, Row} from './styles';
 import { useState } from "react";
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0')
-  const [firstNumber, setFirstNumber] = useState('0')
   const handleAddNumber = (num) =>{
-    setCurrentNumber(prev => `${num}${prev === '0' ? '' : prev}`)
+    if(!isNaN(Number(num))){
+      setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`) 
+    }else{
+      if(num === ","){
+        let lastComma = 0
+        let lastOperationChar = 0
+        for(let i = 0; i < currentNumber.length; i++){
+          if(currentNumber[i] === ","){
+            lastComma = i
+          }else if(isNaN(Number(currentNumber[i]))){
+            lastOperationChar = i
+          }
+        }
+        console.log(lastOperationChar)
+        console.log(lastComma)
+        if(lastOperationChar >= lastComma && currentNumber.length == 0 ? lastOperationChar != (currentNumber.length - 1) : true){
+          setCurrentNumber(prev => `${isNaN(prev[prev.length - 1]) ? prev.substring(0, prev.length - 1) + num : prev + num}`) 
+        }
+      }else{
+        setCurrentNumber(prev => `${isNaN(prev[prev.length - 1]) ? prev.substring(0, prev.length - 1) + num : prev + num}`) 
+      }
+    }
   }
+
   const handleOnClear = () =>{
     setCurrentNumber('0')
   }
 
-  const handleSumNumber = () => {
-    if(firstNumber === '0'){
-      setFirstNumber(currentNumber);
-      handleOnClear()
-    }else{
-      const sum = Number(firstNumber) + Number(currentNumber())
-      setCurrentNumber(String(sum))
-    }
+  const handleOnCalculate = () => {
+    console.log(currentNumber)
   }
 
   return (
@@ -28,16 +43,16 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="C" onClick={() => handleAddNumber('×')}/>
-          <Button label="%" onClick={() => handleAddNumber('÷')}/>
+          <Button label=" "/>
           <Button label="C" onClick={handleOnClear}/>
-          <Button label="÷" onClick={() => handleAddNumber('%')}/>
+          <Button label="%" onClick={() => handleAddNumber('%')}/>
+          <Button label="÷" onClick={() => handleAddNumber('÷')}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="×" onClick={() => handleAddNumber('-')}/>
+          <Button label="×" onClick={() => handleAddNumber('×')}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
@@ -49,13 +64,13 @@ const App = () => {
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="+" onClick={handleSumNumber}/>
+          <Button label="+" onClick={() => handleAddNumber('+')}/>
         </Row>
         <Row>
           <Button label=" "/>
           <Button label="0" onClick={() => handleAddNumber('0')}/>
-          <Button label="." onClick={() => handleAddNumber('.')}/>
-          <Button label="=" onClick={() => handleAddNumber('=')}/>
+          <Button label="," onClick={() => handleAddNumber(',')}/>
+          <Button label="=" onClick={handleOnCalculate}/>
         </Row>
       </Content>
     </Container>
